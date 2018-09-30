6c4143f31292552d7fa5a01596eb2b3447880227
Johnson Li <jli39@stuy.edu>
2018-09-30 15:08:20 -0700

    added rest of functions

--- strings.c
+++ strings.c
@@ -12,11 +12,27 @@ int my_strlen( char *source ){
 
 }
 
-// char *mystrcpy( char *dest, char *source ){
-// 	int counter = my_strlen(*source);
-// 	while(counter--){
-// 		*dest[
-// }
+char * my_strncpy(char *dest, char *source, int n){
+
+	int counter = 0;
+	while(counter <  n && source[counter]){
+		dest[counter++] = source[counter];
+	}
+
+    int i;
+    for(i = counter; i <= n; i++){
+        dest[i] = 0;
+    }
+	// dest[strlen(dest)] = 0; strncpy doesnt auto null terminate
+
+	return dest;
+
+}
+
+char *my_strcpy(char *dest, char *source){
+    dest[strlen(source)] = 0;
+    my_strncpy(dest, source, strlen(source));
+}
 
 char * my_strncat( char *dest, char *source, int n){
 
@@ -46,6 +62,18 @@ int my_strcmp( char *s1, char *s2 ){
 
 }
 
+char * my_strchr( char *s, char c ) {
+    
+    while(*s++){
+        if(*s == c){
+            return s;
+        }
+    }
+
+    return 0;
+
+}
+
 int main(){
 
     char a[25] = "cat";
@@ -65,6 +93,21 @@ int main(){
     printf("Testing strlen(s3):\n");
     printf("[standard]: [%ld]\n", strlen(a));
     printf("[mine]: [%d]\n", my_strlen(a));
+    printf("---------------------------\n");
+    printf("\n");
+
+    printf("---------------------------\n");
+    printf("Testing strcpy(s2, s1):\n");
+    printf("[standard]: [%s]\n", strcpy(b, a));
+    strcpy(b, "dog"); // reset s2
+    printf("[mine]: [%s]\n", my_strcpy(b, a));
+    strcpy(b, "dog"); // reset s2
+    printf("\n");
+    printf("Testing strcpy(s3, s2):\n");
+    printf("[standard]: [%s]\n", strcpy(c, b));
+    strcpy(c, "catdog"); // reset s3
+    printf("[mine]: [%s]\n", my_strcpy(c, b));
+    strcpy(c, "catdog"); // reset s3
     printf("---------------------------\n");
     printf("\n");
 
@@ -95,9 +138,18 @@ int main(){
     printf("Testing strncmp(\"abc\", \"abc\"):\n");
     printf("[standard]: [%d]\n", strcmp("abc", "abc"));
     printf("[mine]: [%d]\n", my_strcmp("abc", "abc"));
-    printf("\n");
     printf("---------------------------\n");
+    printf("\n");
 
+    printf("---------------------------\n");
+    printf("Testing strchr(s1, 'o'):\n");
+    printf("[standard]: [%s]\n", strchr(a, 'o'));
+    printf("[mine]: [%s]\n", my_strchr(a, 'o'));
+    printf("\n");
+    printf("Testing strchr(s3, d):\n");
+    printf("[standard]: [%s]\n", strchr(c, 'd'));
+    printf("[mine]: [%s]\n", my_strchr(c, 'd'));
+    printf("---------------------------\n");
 
     return 0;
 }
